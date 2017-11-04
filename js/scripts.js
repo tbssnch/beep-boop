430e5f57f723f7a9788b16c103dd9778f2b0659d
Tyler Bussanich <tbussanich@gmail.com>
2017-11-04 13:28:34 -0700

    recstruce if/esle if statements so that every number divisible by 3 will output 'I'm sorry Dave, I'm afraid I can't do that.'

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 function numberFunction(userInput) {
   var numbers = [];
   for (var i = 0; i <= userInput; i++) {
-    if (userInput % 3 === 0) {
-      numbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
-    }
-    else if (i === 0) {
-      numbers[i] = "Beep!";
+    if (i === 0) {
+      numbers.push("Beep!");
     }
     else if (i === 1) {
-      numbers[i] = "Boop!";
+      numbers.push("Boop!");
+    }
+    else if (i % 3 === 0) {
+      numbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
     }
     else {
       numbers.push(i);
